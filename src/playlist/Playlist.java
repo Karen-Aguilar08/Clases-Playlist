9fec0e6a7f1c41ce2806395376d96bff50bb6830
Admin <Admin@DESKTOP-7D8APO7>
2025-03-28 14:45:55 -0600

    Clase Playlist

---
+++ src/playlist/Playlist.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package playlist;
+
+/**
+ *
+ * @author Admin
+ */
+public class Playlist {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Musica p1=new Musica("Paso gigante","Cumbias Mexicanas","1987",3);
+        p1.ShowPlaylist();
+    }
+    
+}
